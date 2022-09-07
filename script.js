b58d45652896b8bea215a15efdad9fc8ff3f7441
Lucky Ligovi <luckyligovi@gmail.com>
2022-09-07 17:04:49 +0300

    script

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ fetch('https://fakestoreapi.com/products').then((data)=>{
     console.log(objectData[0].title);
     let tableData = "";
     objectData.map((values)=>{
-        tableData+=`<h2>${values.title}</h2>`;
+         tableData+=`   <tr>
+         <td>Smart phone</td>
+        <td>Samsung</td>
+        <td>Ksh 15,000</td>
+      </tr>
+      <tr>
+        <td>Smart phone</td>
+        <td>Iphone</td>
+        <td>Ksh 35,000</td>
+      </tr>
+      <tr>
+        <td>cloth</td>
+        <td>T-shirt</td>
+        <td>Ksh 1,000</td>
+      </tr>
+      <td>Food</td>
+      <td>Pizaa</td>
+      <td>Ksh 1,200</td>
+    </tr>`;
     })
-    document.getElementById("t-body").innerHTML = tableData
+    document.getElementById("t-body").innerHTML = tableData;
 
 })
