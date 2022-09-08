770aea3fb8c797058cdadd9d39286c388ad60ce1
Lucky Ligovi <luckyligovi@gmail.com>
2022-09-08 09:19:55 +0300

    html changes

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-fetch('https://fakestoreapi.com/products').then((data)=>{
-    return data.json();
-}).then((objectData)=>{
-    console.log(objectData[0].title);
-    let tableData = "";
-    objectData.map((values)=>{
-         tableData+=`   <tr>
-         <td>Smart phone</td>
-        <td>Samsung</td>
-        <td>Ksh 15,000</td>
-      </tr>
-      <tr>
-        <td>Smart phone</td>
-        <td>Iphone</td>
-        <td>Ksh 35,000</td>
-      </tr>
-      <tr>
-        <td>cloth</td>
-        <td>T-shirt</td>
-        <td>Ksh 1,000</td>
-      </tr>
-      <td>Food</td>
-      <td>Pizaa</td>
-      <td>Ksh 1,200</td>
-    </tr>`;
+fetch('https://fakestoreapi.com/products')
+.then((apiData)=>{return apiData.json()})
+.then((objectData)=>{
+    console.log(objectData)
+        let tableData = "";
+        objectData.map((values)=>{
+        tableData+=`
+        <tr>
+            <td>${values.title}</td>
+            <td>${values.description}</td>
+            <td>${values.price}</td>
+            <td><img src="${values.image}"/</td>
+        </tr>`;
     })
+
     document.getElementById("t-body").innerHTML = tableData;
-}).catch((err)=>{
-    HTMLFormControlsCollection.log()
+}).catch((error)=>{
+    console.log(error)
 }) 
