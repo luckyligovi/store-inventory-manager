d0aa2872ff3ddf25152d078060ec344d088928ee
Lucky Ligovi <luckyligovi@gmail.com>
2022-09-08 18:21:46 +0300

    Add search input box

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let products = []
+
 fetch('https://fakestoreapi.com/products')
 .then((apiData)=>{return apiData.json()})
 .then((objectData)=>{
-    console.log(objectData)
         let tableData = "";
+        products = objectData
         objectData.map((values)=>{
         tableData+=`
         <tr>
@@ -16,10 +18,36 @@ fetch('https://fakestoreapi.com/products')
     document.getElementById("t-body").innerHTML = tableData;
 }).catch((error)=>{
     console.log(error)
-}) 
+})
+
+
 document.getElementById("myBtn").addEventListener("click", function() {
     alert("clothes,shoes and electrical gadjets");
   });
   function changeText(id) {
     id.innerHTML = "Store table";
-  }  
+  }
+
+
+formObj = document.getElementById("search")
+formObj.addEventListener("submit", myFunction);
+
+inputValue = document.getElementById("input-value")
+
+function myFunction(e) {
+    e.preventDefault()
+    const data = new FormData(e.target);
+    console.log([...data.entries()]);
+    
+    const filteredProducts = products.filter(
+        (product) => {
+            const title = product.title.toLowerCase()
+            const value = inputValue.value.toLowerCase()
+
+            return title.includes(value)
+    });
+        
+    console.log('>>>>>>>', filteredProducts)
+
+
+};
